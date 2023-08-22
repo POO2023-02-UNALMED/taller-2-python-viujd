df6ec4eb24c878f249f36f06d1506c58660b0356
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-08-22 03:12:44 -0500

    crear clase auto con atributos y metodos

--- main.py
+++ main.py
@@ -9,3 +9,31 @@ class Asiento:
         if color in col_val:
             self.color=color
 
+class Auto:
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+
+    def cantidadAsientos(self):
+        contAsi = 0
+        for asiento in self.asientos:
+            if asiento is not None:
+                contAsi += 1
+        return contAsi
+    
+    def verificarIntegridad(self):
+        if self.registro == self.motor.registro:
+            for asiento in self.asientos:
+                if asiento is not None:
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
